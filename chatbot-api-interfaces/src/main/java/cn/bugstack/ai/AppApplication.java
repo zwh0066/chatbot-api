0fa10bea6da4e8cf742c307268816bce07748615
Cyril <1330624982@qq.com>
2023-02-15 23:07:22 +0800

    cyril：init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/ai/AppApplication.java
@@ -0,0 +1,14 @@
+package cn.bugstack.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class,args);
+    }
+}
